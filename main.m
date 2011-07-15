d82eb5318a6993954df1cf22decb590f2686635d
doug <s@ss-MacBook-Pro.local>
2011-07-15 15:59:51 -0600

    first commit

---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  soccer
+//
+//  Created by doug on 6/28/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+	NSAutoreleasePool *pool = [NSAutoreleasePool new];
+	int retVal = UIApplicationMain(argc, argv, nil, @"soccerAppDelegate");
+	[pool release];
+	return retVal;
+}
